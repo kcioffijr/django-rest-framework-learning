f82486da47a4d10e7a49831944993f8709afc2da
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2023-12-12 15:28:55 -0500

    Adding categories field to the MovieData model and serializer.

--- movies/models.py
+++ movies/models.py
@@ -9,3 +9,4 @@ class MovieData(models.Model):
     name = models.CharField(max_length=200)
     duration = models.FloatField()
     rating = models.FloatField()
+    category = models.CharField(max_length=200, default='N/A')
